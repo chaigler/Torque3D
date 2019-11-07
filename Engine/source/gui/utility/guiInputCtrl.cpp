c8a0da4a0e8c85c7ed86c7fd63a512ab03ca005e
chaigler <chaigler@gmail.com>
2019-11-06 22:28:55 -0500

    Broke it, now I fixed it.
    Missed a change for dumb reasons. Oops.

--- Engine/source/gui/utility/guiInputCtrl.cpp
+++ Engine/source/gui/utility/guiInputCtrl.cpp
@@ -26,41 +26,57 @@
 
 IMPLEMENT_CONOBJECT(GuiInputCtrl);
 
-ConsoleDocClass( GuiInputCtrl,
-	"@brief A control that locks the mouse and reports all keyboard input events to script.\n\n"
+ConsoleDocClass(GuiInputCtrl,
+   "@brief A control that locks the mouse and reports all keyboard input events to script.\n\n"
 
-	"This is useful for implementing custom keyboard handling code, and most commonly "
-	"used in Torque for a menu that allows a user to remap their in-game controls\n\n "
+   "This is useful for implementing custom keyboard handling code, and most commonly "
+   "used in Torque for a menu that allows a user to remap their in-game controls\n\n "
 
-	"@tsexample\n"
-	"new GuiInputCtrl(OptRemapInputCtrl)\n"
-	"{\n"
-	"	lockMouse = \"0\";\n"
-	"	position = \"0 0\";\n"
-	"	extent = \"64 64\";\n"
-	"	minExtent = \"8 8\";\n"
-	"	horizSizing = \"center\";\n"
-	"	vertSizing = \"bottom\";\n"
-	"	profile = \"GuiInputCtrlProfile\";\n"
-	"	visible = \"1\";\n"
-	"	active = \"1\";\n"
-	"	tooltipProfile = \"GuiToolTipProfile\";\n"
-	"	hovertime = \"1000\";\n"
-	"	isContainer = \"0\";\n"
-	"	canSave = \"1\";\n"
-	"	canSaveDynamicFields = \"0\";\n"
-	"};\n"
-	"@endtsexample\n\n"
+   "@tsexample\n"
+   "new GuiInputCtrl(OptRemapInputCtrl)\n"
+   "{\n"
+   "	lockMouse = \"0\";\n"
+   "	position = \"0 0\";\n"
+   "	extent = \"64 64\";\n"
+   "	minExtent = \"8 8\";\n"
+   "	horizSizing = \"center\";\n"
+   "	vertSizing = \"bottom\";\n"
+   "	profile = \"GuiInputCtrlProfile\";\n"
+   "	visible = \"1\";\n"
+   "	active = \"1\";\n"
+   "	tooltipProfile = \"GuiToolTipProfile\";\n"
+   "	hovertime = \"1000\";\n"
+   "	isContainer = \"0\";\n"
+   "	canSave = \"1\";\n"
+   "	canSaveDynamicFields = \"0\";\n"
+   "};\n"
+   "@endtsexample\n\n"
 
-	"@see GuiMouseEventCtrl\n"
+   "@see GuiMouseEventCtrl\n"
 
-	"@ingroup GuiUtil\n");
+   "@ingroup GuiUtil\n");
+
+//------------------------------------------------------------------------------
+
+GuiInputCtrl::GuiInputCtrl()
+   : mSendAxisEvents(false),
+   mSendBreakEvents(false),
+   mSendModifierEvents(false)
+{
+}
 
 //------------------------------------------------------------------------------
 
 void GuiInputCtrl::initPersistFields()
 {
-   
+   addGroup("GuiInputCtrl");
+   addField("sendAxisEvents", TypeBool, Offset(mSendAxisEvents, GuiInputCtrl),
+      "If true, onAxisEvent callbacks will be sent for SI_AXIS Move events (Default false).");
+   addField("sendBreakEvents", TypeBool, Offset(mSendBreakEvents, GuiInputCtrl),
+      "If true, break events for all devices will generate callbacks (Default false).");
+   addField("sendModifierEvents", TypeBool, Offset(mSendModifierEvents, GuiInputCtrl),
+      "If true, Make events will be sent for modifier keys (Default false).");
+   endGroup("GuiInputCtrl");
 
    Parent::initPersistFields();
 }
@@ -70,23 +86,23 @@ void GuiInputCtrl::initPersistFields()
 bool GuiInputCtrl::onWake()
 {
    // Set the default profile on start-up:
-   if( !mProfile )
+   if (!mProfile)
    {
       GuiControlProfile* profile;
-      Sim::findObject( "GuiInputCtrlProfile", profile);
-      if( profile )
-         setControlProfile( profile );
+      Sim::findObject("GuiInputCtrlProfile", profile);
+      if (profile)
+         setControlProfile(profile);
    }
 
-   if ( !Parent::onWake() )
-      return( false );
+   if (!Parent::onWake())
+      return(false);
 
-   if( !smDesignTime )
+   if (!smDesignTime)
       mouseLock();
-      
+
    setFirstResponder();
 
-   return( true );
+   return(true);
 }
 
 
@@ -100,29 +116,37 @@ void GuiInputCtrl::onSleep()
 
 
 //------------------------------------------------------------------------------
-static bool isModifierKey( U16 keyCode )
+static bool isModifierKey(U16 keyCode)
 {
-   switch ( keyCode )
+   switch (keyCode)
    {
-      case KEY_LCONTROL:
-      case KEY_RCONTROL:
-      case KEY_LALT:
-      case KEY_RALT:
-      case KEY_LSHIFT:
-      case KEY_RSHIFT:
-         return( true );
+   case KEY_LCONTROL:
+   case KEY_RCONTROL:
+   case KEY_LALT:
+   case KEY_RALT:
+   case KEY_LSHIFT:
+   case KEY_RSHIFT:
+   case KEY_MAC_LOPT:
+   case KEY_MAC_ROPT:
+      return(true);
    }
 
-   return( false );
+   return(false);
 }
 
-IMPLEMENT_CALLBACK( GuiInputCtrl, onInputEvent, void, (const char* device, const char* action, bool state ),
-														  ( device, action, state),
-	"@brief Callback that occurs when an input is triggered on this control\n\n"
-	"@param device The device type triggering the input, such as keyboard, mouse, etc\n"
-	"@param action The actual event occuring, such as a key or button\n"
-	"@param state True if the action is being pressed, false if it is being release\n\n"
-);
+IMPLEMENT_CALLBACK(GuiInputCtrl, onInputEvent, void, (const char* device, const char* action, bool state),
+(device, action, state),
+"@brief Callback that occurs when an input is triggered on this control\n\n"
+"@param device The device type triggering the input, such as keyboard, mouse, etc\n"
+"@param action The actual event occuring, such as a key or button\n"
+"@param state True if the action is being pressed, false if it is being release\n\n");
+
+IMPLEMENT_CALLBACK(GuiInputCtrl, onAxisEvent, void, (const char* device, const char* action, F32 axisValue),
+(device, action, axisValue),
+"@brief Callback that occurs when an axis event is triggered on this control\n\n"
+"@param device The device type triggering the input, such as mouse, joystick, gamepad, etc\n"
+"@param action The ActionMap code for the axis\n"
+"@param axisValue The current value of the axis\n\n");
 
 //------------------------------------------------------------------------------
 bool GuiInputCtrl::onInputEvent(const InputEventInfo& event)
